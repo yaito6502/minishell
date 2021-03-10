9c0dd8849194ddd672ec34c3b84a998a1e68dde6
Ynakamot <nakamo326@gmail.com>
2021-03-10 14:40:19 +0900

    [add] create_new_tcommand()追加 (#8)
    初期値のt_commandを返す関数です。
    テストコードをsrcs/testsに追加しています。

---
+++ srcs/utils/create_new_tcommand.c
@@ -0,0 +1,23 @@
+#include "minishell.h"
+
+/*
+** 新しいt_commandを作成、そのポインター型変数を返す。
+*/
+
+t_command	*create_new_tcommand(void)
+{
+	t_command *ptr;
+
+	if (!(ptr = malloc(sizeof(t_command))))
+		return (NULL);
+	ptr->next = NULL;
+	ptr->argv = NULL;
+	ptr->redirect_in = NULL;
+	ptr->redirect_out = NULL;
+	ptr->receive_pipe = false;
+	ptr->lastfd[0] = -1;
+	ptr->lastfd[1] = -1;
+	ptr->pid = -1;
+	ptr->op = EOS;
+	return (ptr);
+}
