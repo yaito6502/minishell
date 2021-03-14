09b2f9706a75fbe4548061c599e50643afb98600
Ynakamot <nakamo326@gmail.com>
2021-03-15 01:01:04 +0900

    [fix]do_redirection修正 (#25)
    * [add] do_redirection.c追加
    
    リダイレクトを行う関数を追加しました。
    エラー処理は、プロセスで処理が変わるため、現在は暫定的な出力のみです。
    テストも正常系のみになります。
    
    * [add] do_redirection.c
    
    <,>,>>に対応した、リダイレクトを行う関数です。

--- tests/execute/test_do_redirection.c
+++ tests/execute/test_do_redirection.c
@@ -8,15 +8,25 @@ int main(void)
 	int fd;
 
 	cmd = create_new_tcommand();
-	cmd->redirect_in = ft_split("./testinout/infile1,./testinout/infile2",',');
-	cmd->redirect_out = ft_split(">,testinout/outfile1,500>,testinout/outfile500",',');
+	cmd->redirect_in = ft_split("<,./testinout/infile1,30<,./testinout/infile2",',');
+	cmd->redirect_out = ft_split(">,testinout/outfile1,500>>,testinout/outfile500",',');
 	print_tcommand(*cmd);
 	redirect_input(cmd);
 	redirect_output(cmd);
 
+	//fd 0 to 1
 	rc = read(STDIN_FILENO, buf, 24);
 	if (rc == -1)
 		printf("%s\n", strerror(errno));
 	buf[rc] = '\0';
+	write(1, buf, rc);
+
+	//fd 30 to 500
+	rc = read(30, buf, 24);
+	if (rc == -1)
+		printf("%s\n", strerror(errno));
+	buf[rc] = '\0';
 	write(500, buf, rc);
+
+	return 0;
 }
