72c8a555be427b9e9e74469bb94309b4be351129
Ynakamot <nakamo326@gmail.com>
2021-03-22 14:39:09 +0900

    [add] execute_builtin() 追加 (#35)
    * [add, fix] join_path()追加
    
    相対パスとカレントディレクトリから絶対パスを生成する関数を追加しました。makefileのTESTCOREの削除も併せて行っています。
    
    * [add] has_slash() 追加
    
    文字列内に'/'があるかを判定する関数を追加しました。
    
    * [add] execute_parallel()　追加
    
    コマンドをパラレルに実行する関数を追加しました。実行ファイルパスを探すための関数をexecute_sequential関数にも追加しています。
    
    * [add] reconnect_stdfd()　追加
    
    関数内に標準入出力を保存し、復元するための関数を追加しました。
    
    * [add] start_commands()　追加
    
    コマンドリストを受け取って実行する関数を追加しました。
    
    * [add] execute_builtin() 追加
    
    コマンドに合わせて各ビルトインコマンド関数を呼び出す関数です。
    現状テストをする意味がないと思うので、各ビルトイン関数ができてからテストしようと思います。
    
    * Update Makefile
    
    * [fix] 関数ポインタ配列の使用へ変更
    
    コメントで指摘いただいたように関数ポインタを使用する形へ変更しました。
    
    * [fix] retの範囲をチェックするif文を追加
    
    execute_builtin()内にて、retが存在するindexの範囲内であった場合のみ関数を実行するように修正を加えました。
    
    Co-authored-by: yaito6502 <schwarzekatze8010110@gmail.com>
    Co-authored-by: yaito <79784339+yaito6502@users.noreply.github.com>

---
+++ srcs/builtin/execute_builtin.c
@@ -0,0 +1,20 @@
+#include "minishell.h"
+
+void	execute_builtin(t_command *cmd)
+{
+	int			ret;
+	const void	(*builtinfunc[7])(t_command*) = {
+		execute_echo(),
+		execute_cd(),
+		execute_pwd(),
+		execute_export(),
+		execute_unset(),
+		execute_env(),
+		execute_exit()
+	};
+
+	ret = is_builtin(cmd);
+	if (ret >= 0 && ret <= 6)
+		builtinfunc[ret](cmd);
+	return ;
+}
