f2b430dc45d1e718dd0537eee958b70fca6ee68a
yaito <79784339+yaito6502@users.noreply.github.com>
2021-03-25 17:27:52 +0900

    [fix,update] normV3に備えたコードの修正、エラー出力機能追加 (#53)
    if文の中で代入を行わないようにしました。入力が不正な時にbashと同じエラー出力をするように変更しました。
    また、全てのボーナスに対応させています。

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -7,7 +7,8 @@ static t_command	*get_command(char **strs)
 
 	if (!strs)
 		return (NULL);
-	if ((cmd = create_new_tcommand()) == NULL)
+	cmd = create_new_tcommand();
+	if (cmd == NULL)
 		return (NULL);
 	i = 0;
 	while (strs[i] != NULL)
@@ -16,13 +17,12 @@ static t_command	*get_command(char **strs)
 		{
 			if (!set_redirection_list(cmd, strs + i))
 				return (wrap_free_commands_list(cmd));
-			if (ft_strchr(strs[i + 1], '<') || ft_strchr(strs[i + 1], '>'))
-				i++;
 			i += 2;
 		}
 		else
 		{
-			if ((cmd->argv = add_str_to_list(cmd->argv, strs[i++])) == NULL)
+			cmd->argv = add_str_to_list(cmd->argv, strs[i++]);
+			if (cmd->argv == NULL)
 				return (wrap_free_commands_list(cmd));
 		}
 	}
@@ -38,10 +38,21 @@ static t_command	*get_pipeline(char **strs)
 		return (NULL);
 	if ((i = strschr(strs, "|")) < 0)
 		return (get_command(strs));
-	if ((pipeline = get_pipeline(get_strs(strs, i))) == NULL)
+	pipeline = NULL;
+	if (i == 0)
+		printf("bash: syntax error near unexpected token `%s'\n", strs[i]);
+	else
+		pipeline = get_pipeline(get_strs(strs, i));
+	if (pipeline == NULL)
 		return (NULL);
 	pipeline->op = PIPELINE;
-	if ((pipeline->next = get_pipeline(get_strs(strs + i + 1, 0))) == NULL)
+	if (strs[i + 1] == NULL)
+	{
+		printf("bash: syntax error near unexpected token `%s'\n", strs[i]);
+		return (NULL);
+	}
+	pipeline->next = get_pipeline(get_strs(strs + i + 1, 0));
+	if (pipeline->next == NULL)
 		return (wrap_free_commands_list(pipeline));
 	pipeline->next->receive_pipe = true;
 	return (pipeline);
@@ -57,14 +68,25 @@ static t_command	*get_andor(char **strs)
 	if ((i = strschr(strs, "&|")) < 0 || (!ft_strchr(strs[i + 1], '&') && \
 	!ft_strchr(strs[i + 1], '|')))
 		return (get_pipeline(strs));
-	if ((andor = get_andor(get_strs(strs, i))) == NULL)
+	andor = NULL;
+	if (i == 0)
+		printf("bash: syntax error near unexpected token `%s'\n", strs[i]);
+	else
+		andor = get_andor(get_strs(strs, i));
+	if (andor == NULL)
 		return (NULL);
 	/*if (ft_strchr(strs[i + 1], '&'))
 	**	andor.? = AND;
 	**else
 	**	andor.? = PIPELINE;
 	*/
-	if ((andor->next = get_andor(get_strs(strs + i + 1, 0))) == NULL)
+	if (strs[i + 1] == NULL)
+	{
+		printf("bash: syntax error near unexpected token `%s'\n", strs[i]);
+		return (NULL);
+	}
+	andor->next = get_andor(get_strs(strs + i + 1, 0));
+	if (andor->next == NULL)
 		return (wrap_free_commands_list(andor));
 	return (andor);
 }
@@ -78,14 +100,23 @@ static t_command	*get_list(char **strs)
 		return (NULL);
 	if ((i = strschr(strs, ";&")) < 0)
 		return (get_andor(strs));
-	if ((list = get_list(get_strs(strs, i))) == NULL)
+	list = NULL;
+	if (i == 0)
+		printf("bash: syntax error near unexpected token `%s'\n", strs[i]);
+	else
+		list = get_list(get_strs(strs, i));
+	if (list == NULL)
 		return (NULL);
 	if (ft_strchr(strs[i], ';'))
 		list->op = SCOLON;
 	/*else
 	**	list->op = AND;
 	*/
-	if ((list->next = get_list(get_strs(strs + i + 1, 0))) == NULL)
+	if (strs[i + 1] == NULL)
+		printf("bash: syntax error near unexpected token `%s'\n", strs[i]);
+	else
+		list->next = get_list(get_strs(strs + i + 1, 0));
+	if (list->next == NULL)
 		return (wrap_free_commands_list(list));
 	return (list);
 }
@@ -103,6 +134,8 @@ t_command			*get_commandline(char **strs)
 		cmds = get_list(get_strs(strs, -1));
 	else
 		cmds = get_list(strs);
+	if (cmds == NULL)
+		return (NULL);
 	head = cmds;
 	while (cmds->next)
 		cmds = cmds->next;
