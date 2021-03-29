0fa8e7a1e83ff9cc76aef7c9c4d21c6cc7760744
yaito6502 <schwarzekatze8010110@gmail.com>
2021-03-29 16:43:03 +0900

    [change] error_execute()変更
    errnoマクロを使用する様に変更しました。この関数は今後拡張する可能性が高いです。

--- srcs/execute/error_execute.c
+++ srcs/execute/error_execute.c
@@ -5,11 +5,13 @@ int		error_execute(char *path)
 	ft_putstr_fd("bash: ", 2);
 	ft_putstr_fd(path, 2);
 	ft_putstr_fd(": ", 2);
-	if (errno == 14)
+	if (errno == EFAULT)
 		ft_putendl_fd("command not found", 2);
 	else
 		ft_putendl_fd(strerror(errno), 2);
-	if (errno == 13)
+	if (errno == EACCES)
 		return (126);
-	return (127);
+	if (errno == EFAULT)
+		return (127);
+	return (1);
 }
