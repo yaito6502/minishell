f6244ef7c9bb1e3224dbe11174ba9978fd1282de
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-02 19:49:55 +0900

    [fix] クォート処理修正
    クォートの処理を修正しました。

--- srcs/parser/expand_envval.c
+++ srcs/parser/expand_envval.c
@@ -49,14 +49,18 @@ static char	*copy_literal(char *line, char *ret, int *i)
 	return (ret);
 }
 
-static char	*copy_normalchar(char *line, char *ret, int *i)
+static char	*copy_normalchar(char *line, char *ret, int *i, bool inquote)
 {
 	char *tmp;
 	char *str;
 
 	*i = 0;
-	while (line[*i] != '\'' && line[*i] != '\0' && line[*i] != '$')
-		(*i)++;
+	if (inquote == true)
+		while (line[*i] != '\0' && line[*i] != '$')
+			(*i)++;
+	else
+		while (line[*i] != '\'' && line[*i] != '\0' && line[*i] != '$')
+			(*i)++;
 	str = ft_substr(line, 0, *i);
 	if (str == NULL)
 		return (NULL);
@@ -106,18 +110,24 @@ char		*expand_envval(char *line)
 {
 	char	*ret;
 	int		i;
+	bool	inquote;
 
+	inquote = false;
 	if (has_dollar(line) == 0)
 		return (line);
 	ret = NULL;
 	while (*line != '\0')
 	{
-		if (*line == '\'')
+		if (*line == '"' && inquote == false)
+			inquote = true;
+		else if (*line == '"' && inquote == true)
+			inquote = false;
+		if (*line == '\'' && inquote == false)
 			ret = copy_literal(line, ret, &i);
 		else if (*line == '$')
 			ret = get_key(line, ret, &i);
 		else
-			ret = copy_normalchar(line, ret, &i);
+			ret = copy_normalchar(line, ret, &i, inquote);
 		if (ret == NULL)
 			return (NULL);
 		line = line + i + 1;
