a6ec5ea7092f302ba96625ad81fa3a19dc17aabb
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-03 15:13:58 +0900

    [fix] return修正

--- srcs/execute/connect_pipeline.c
+++ srcs/execute/connect_pipeline.c
@@ -3,14 +3,14 @@
 static bool	error_piping(void)
 {
 	ft_putstr_fd("minishell: pipe: ", 2);
-	ft_putstr_fd(strerror(errno), 2);
+	ft_putendl_fd(strerror(errno), 2);
 	return (false);
 }
 
 bool		send_pipeline(t_command *cmd, int newpipe[2])
 {
 	if (cmd->op != PIPELINE)
-		return ;
+		return (true);
 	if (close(newpipe[0]) == -1)
 		return (error_piping());
 	if (dup2(newpipe[1], STDOUT_FILENO) == -1)
@@ -25,7 +25,7 @@ bool		receive_pipeline(t_command *cmd)
 	int	ret;
 
 	if (cmd->receive_pipe == false)
-		return ;
+		return (true);
 	if (close(cmd->lastfd[1]) == -1)
 		return (error_piping());
 	if (dup2(cmd->lastfd[0], STDIN_FILENO) == -1)
@@ -34,7 +34,3 @@ bool		receive_pipeline(t_command *cmd)
 		return (error_piping());
 	return (true);
 }
-
-/*
-** dup2のエラーキャッチが必要かも。要検討
-*/
