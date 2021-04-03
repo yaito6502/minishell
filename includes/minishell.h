d736ae555455792a0cd22b186f9f320490d84796
Ynakamot <nakamo326@gmail.com>
2021-04-03 15:14:28 +0900

    [update] リダイレクトエラー出力追加 (#73)
    fdが不正なパターンのエラー出力を追加しました。
    引数の不正なパターンには未対応です。
    
    Co-authored-by: yaito <79784339+yaito6502@users.noreply.github.com>

--- includes/minishell.h
+++ includes/minishell.h
@@ -87,22 +87,21 @@ bool			set_redirection_list(t_command *cmd, char **list);
 char			*get_cmd_frompath(t_command *cmd);
 void			send_pipeline(t_command *cmds, int newpipe[2]);
 void			receive_pipeline(t_command *cmds);
-void			redirect_input(t_command *cmds);
-void			redirect_output(t_command *cmds);
+bool			redirect_input(t_command *cmds);
+bool			redirect_output(t_command *cmds);
 bool			reconnect_stdfd(int mode);
 char			*get_cmd_frompath(t_command *cmd);
 char			*join_path(char *cmd);
 void			execute_sequential(t_command *cmd);
 void			execute_parallel(t_command *cmd);
 void			start_commands(t_command *cmd);
-int				error_execute(char *path);
 int				store_exitstatus(int mode, int last_status);
 
 //builtin
 void			execute_builtin(t_command *cmd);
 int				execute_echo(t_command *cmd);
 int				execute_env(t_command *cmd);
-int			  execute_unset(t_command *cmd);
+int				execute_unset(t_command *cmd);
 int				execute_pwd(t_command *cmd);
 
 //parser
@@ -120,6 +119,11 @@ bool		reset_terminal_setting(void);
 //history
 t_history		*add_history(t_history *last_history, char *line);
 void			free_history(t_history *history);
+
+//error output
+int				error_execute(char *path);
+bool			redirect_error(char *key, char *errmsg);
+bool			fd_error(long fd, char *errmsg);
 
 //terminal setting and termcap
 char		*read_line(void);
