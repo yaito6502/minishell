055f4a340093fcdad28b41b7ada5fce58a3426f5
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-05 11:26:15 +0900

    [fix]エラー出力修正
    エラー出力内でmallocを使わないよう変更しました。

--- srcs/execute/error_redirect.c
+++ srcs/execute/error_redirect.c
@@ -14,21 +14,15 @@ bool	redirect_error(char *key, char *errmsg)
 
 bool	fd_error(long fd, char *errmsg)
 {
-	char	*str;
-
-	if (fd < INT_MAX)
-		str = ft_ltoa(fd);
-	else
-		str = ft_strdup("file descriptor out of range");
-	if (!str)
-		return (false);
 	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(str, 2);
+	if (fd < INT_MAX)
+		ft_putnbr_fd((int)fd, 2);
+	else
+		ft_putstr_fd("file descriptor out of range", 2);
 	ft_putstr_fd(": ", 2);
 	if (errmsg == NULL)
 		ft_putendl_fd(strerror(errno), 2);
 	else
 		ft_putendl_fd(errmsg, 2);
-	free(str);
 	return (false);
 }
