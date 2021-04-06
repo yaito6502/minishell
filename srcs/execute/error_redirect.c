b2283944255e9318749aa700604f471e9a7da606
yaito6502 <schwarzekatze8010110@gmail.com>
2021-04-06 17:01:32 +0900

    [change] エラー出力のfdにSTDERRマクロを使用するように変更。

--- srcs/execute/error_redirect.c
+++ srcs/execute/error_redirect.c
@@ -2,27 +2,27 @@
 
 bool	redirect_error(char *key, char *errmsg)
 {
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(key, 2);
-	ft_putstr_fd(": ", 2);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(key, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
 	if (errmsg == NULL)
-		ft_putendl_fd(strerror(errno), 2);
+		ft_putendl_fd(strerror(errno), STDERR_FILENO);
 	else
-		ft_putendl_fd(errmsg, 2);
+		ft_putendl_fd(errmsg, STDERR_FILENO);
 	return (false);
 }
 
 bool	fd_error(long fd, char *errmsg)
 {
-	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	if (fd < INT_MAX)
-		ft_putnbr_fd((int)fd, 2);
+		ft_putnbr_fd((int)fd, STDERR_FILENO);
 	else
-		ft_putstr_fd("file descriptor out of range", 2);
-	ft_putstr_fd(": ", 2);
+		ft_putstr_fd("file descriptor out of range", STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
 	if (errmsg == NULL)
-		ft_putendl_fd(strerror(errno), 2);
+		ft_putendl_fd(strerror(errno), STDERR_FILENO);
 	else
-		ft_putendl_fd(errmsg, 2);
+		ft_putendl_fd(errmsg, STDERR_FILENO);
 	return (false);
 }
