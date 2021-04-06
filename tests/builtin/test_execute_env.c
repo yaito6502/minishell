76df81684d91d6db79315beafe694ec5b1f0846b
yaito6502 <schwarzekatze8010110@gmail.com>
2021-04-06 13:51:19 +0900

    [update] テストケース変更
    テストの中で環境変数を追加し、envの動作を確認するようにしました。

--- tests/builtin/test_execute_env.c
+++ tests/builtin/test_execute_env.c
@@ -4,6 +4,9 @@ int main(void)
 {
 	t_command *cmd;
 
+	create_newenv();
+	add_newval_to_env("arg");
+	add_newval_to_env("test=1");
 	cmd = create_new_tcommand();
 	cmd->argv = ft_split("env,arg",',');
 	execute_env(cmd);
