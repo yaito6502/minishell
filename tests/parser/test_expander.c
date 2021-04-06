a0eb18e39b7747b0b860215b7b69be843243b05c
yaito6502 <schwarzekatze8010110@gmail.com>
2021-04-06 15:14:53 +0900

    [add]　test_expander.sh追加
    テスト用シェルを追加しました。また、$が奇数個連続続く文字列の時にメモリリークを発見しました。

--- tests/parser/test_expander.c
+++ tests/parser/test_expander.c
@@ -1,16 +1,16 @@
 #include "minishell.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
 	t_command	*cmd;
 	bool		ret;
-	size_t		i = 0;
 	// export TEST="$TERM + $PWD"
 
 	store_exitstatus(SAVE, 1);
 	cmd = create_new_tcommand();
+	cmd->argv = ft_split(argv[1], ',');
 	//cmd->argv = ft_split("'$?',$?,\"$?\",\"world's end\",'$HOME'aaaa\"$TEST\",$PWD",',');
-	cmd->argv = ft_split("\"'$?$?'\",'$?',$??,\"world's end\",'$HOME'aaaa\"$TEST\",$PWD",',');
+	//cmd->argv = ft_split("\"'$?$?'\",'$?',$??,\"world's end\",'$HOME'aaaa\"$TEST\",$PWD",',');
 	///*while (cmd->argv[i] != NULL)
 	//	puts(cmd->argv[i++]);*/
 	ret = preprocess_command(cmd);
@@ -20,5 +20,6 @@ int main(void)
 		puts("failure");
 	print_tcommand(*cmd);
 	free_commandslist(&cmd);
+	system("leaks test");
 	return 0;
 }
