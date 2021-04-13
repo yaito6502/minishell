e41eb179aa72f425bfb411f897e5c90329974a9e
Ynakamot <nakamo326@gmail.com>
2021-04-13 13:38:51 +0900

    [add]コマンド実行中シグナル処理実装 (#89)
    * [fix]main関数修正。その他軽微な調整
    
    マージされた関数をすべて処理に組み込みました。
    その他、デバッグ出力、未使用関数の削除など行っています。
    
    * [add] コマンド実行中シグナル追加
    
    * マージ修正
    
    * [fix]start_command修正
    
    * [fix] ctrl-\の動作を改善
    
    * [fix] read_line中ctrl-\入力無視
    
    * [add] Ctrl-\シグナル時の(core dump)出力追加
    
    * [fix] WCOREDUMPをWIFSIGNALED後に移動
    
    Co-authored-by: yaito6502 <schwarzekatze8010110@gmail.com>

--- includes/minishell.h
+++ includes/minishell.h
@@ -103,6 +103,8 @@ int				execute_sequential(t_command *cmd);
 int				execute_parallel(t_command *cmd);
 void			start_commands(t_command *cmd);
 int				store_exitstatus(int mode, int last_status);
+void			sigint_handler(int signal);
+void			sigquit_handler(int signal);
 
 //builtin
 int				execute_builtin(t_command *cmd);
