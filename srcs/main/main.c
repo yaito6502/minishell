acb48ee2548f30621a916604893f3bb216a5edd0
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-18 10:57:08 +0900

    [add]cオプション、pipeからの入力に対応
    cオプション、pipeからの入力でのコマンド実行を追加しました。

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -36,7 +36,7 @@ static void	wait_command(t_hist **hist)
 	char		**tokens;
 	t_command	*cmd;
 
-	write(STDOUT_FILENO, "\033[34mminishell\033[m > ", 21);
+	write(STDERR_FILENO, "\033[34mminishell\033[m > ", 21);
 	*hist = add_newelm_to_hist(*hist);
 	line = read_line(hist);
 	line = check_validline(line);
@@ -55,17 +55,46 @@ static void	wait_command(t_hist **hist)
 	return ;
 }
 
-int	main(void)
+static void	run_command(char *line)
+{
+	char		**tokens;
+	t_command	*cmd;
+
+	line = check_validline(line);
+	if (line == NULL)
+		exit(2);
+	tokens = tokenize(line);
+	free(line);
+	cmd = parse(tokens);
+	ft_free_split(tokens);
+	if (cmd == NULL)
+		exit(EXIT_FAILURE);
+	start_commands(cmd);
+	free_commandslist(&cmd);
+	exit(store_exitstatus(LOAD, 0));
+}
+
+int	main(int argc, char **argv)
 {
 	extern t_termcap	term;
 	t_hist				*hist;
+	char				*line;
 
 	hist = NULL;
 	create_newenv();
 	init_tterm();
+	if (argc > 2 && !ft_strncmp(argv[1], "-c", 3))
+	{
+		line = ft_strdup(argv[2]);
+		run_command(line);
+	}
+	if (!isatty(STDIN_FILENO))
+	{
+		line = read_command();
+		run_command(line);
+	}
 	get_terminal_description();
 	set_termcapsettings(term);
-	printf("Hello, welcome to our minishell!\n");
 	while (1)
 		wait_command(&hist);
 	return (0);
