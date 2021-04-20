229457da71cfb1e4a384b34639e92b2461a8b84f
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-20 10:03:33 +0900

    [fix]error_execute修正
    error_execute関数実行中にエラーが発生したときerrnoが変更され期待した動作ができていなかった問題を修正

--- srcs/execute/error_execute.c
+++ srcs/execute/error_execute.c
@@ -1,17 +1,17 @@
 #include "minishell.h"
 
-int	error_execute(char *path)
+int	error_execute(char *path, int last_errno)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd(path, STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
-	if (errno == EFAULT)
+	if (last_errno == EFAULT)
 		ft_putendl_fd("command not found", STDERR_FILENO);
 	else
-		ft_putendl_fd(strerror(errno), STDERR_FILENO);
-	if (errno == EACCES)
+		ft_putendl_fd(strerror(last_errno), STDERR_FILENO);
+	if (last_errno == EACCES)
 		return (126);
-	if (errno == ENOENT || errno == EFAULT)
+	if (last_errno == ENOENT || last_errno == EFAULT)
 		return (127);
 	return (1);
 }
