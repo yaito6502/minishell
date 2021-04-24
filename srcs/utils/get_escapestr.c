6156498e50a35aa6a6f946d52a147ebdfec0e0d7
Ynakamot <nakamo326@gmail.com>
2021-04-25 02:53:15 +0900

    [add]バックスラッシュエスケープ追加、Makefile修正 (#110)
    * [add]get_escapestr.c
    
    * fix typo
    
    * fix typo
    
    * エラーメッセージ修正
    
    * [change] unsetでのエラー時にEXIT_FAIUREマクロを使用するように変更
    
    Co-authored-by: yaito6502 <schwarzekatze8010110@gmail.com>

---
+++ srcs/utils/get_escapestr.c
@@ -0,0 +1,43 @@
+#include "minishell.h"
+
+static int	get_len(char *line)
+{
+	int	len;
+
+	len = 0;
+	while (*line)
+	{
+		if (*line == '\\')
+		{
+			line++;
+			continue ;
+		}
+		len++;
+		line++;
+	}
+	return (len);
+}
+
+char	*get_escapestr(char *line)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = get_len(line);
+	str = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (*line != '\0')
+	{
+		if (*line == '\\')
+		{
+			line++;
+			continue ;
+		}
+		str[i] = *line;
+		i++;
+		line++;
+	}
+	str[i] = '\0';
+	return (str);
+}
