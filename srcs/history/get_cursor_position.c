43a8dc010a09298cd3ca9164a7dee7444ee02e99
Ynakamot <nakamo326@gmail.com>
2021-05-13 17:13:47 +0900

    [fix]get_cursor_position修正 (#142)
    linux環境での動作を改善しました。

--- srcs/history/get_cursor_position.c
+++ srcs/history/get_cursor_position.c
@@ -6,8 +6,8 @@ void	get_cursor_position(int *row, int *col)
 	char	buf[255];
 	int		rc;
 
-	write(0, "\033[6n", 4);
-	rc = read(0, buf, 254);
+	write(STDIN_FILENO, "\033[6n", 4);
+	rc = read(STDOUT_FILENO, buf, 254);
 	buf[rc] = '\0';
 	i = 1;
 	while (buf[i] != '\0' && !ft_isdigit(buf[i]))
